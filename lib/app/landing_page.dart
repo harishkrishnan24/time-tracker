d55818d87edb02f046a5f490b2fe8ec18fb40dc7
harishkrishnan1993 <harishkrishnan1993@gmail.com>
2020-01-22 17:24:11 +0530

    Added streams to auth

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class LandingPage extends StatelessWidget {
+  final AuthBase auth;
+
+  LandingPage({@required this.auth});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User>(
+      stream: auth.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          User user = snapshot.data;
+          if (user == null) {
+            return SignInPage(auth: auth);
+          }
+          return HomePage(auth: auth);
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
